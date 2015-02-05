706f6aa057139a6b67f9eb54a8d0a419bcb5dafc
mizutaki <mizutaki4@gmail.com>
2015-02-05 13:02:32 +0900

    try and error

--- http_request.rb
+++ http_request.rb
@@ -3,14 +3,24 @@ require 'open-uri'
 require 'pp'
 require 'json'
 
+class Event
+  attr_accessor :url, :title, :description
+end
 #connpassAPIをたたくHTTPクライアント
-html = open('http://connpass.com/api/v1/event/?keyword=python').read
+html = open('http://connpass.com/api/v1/event/?keyword=python&count=2').read
 json = JSON.parser.new(html)
 hash = json.parse()
-pp hash
-#イベント毎に分割して、それぞれ処理していく
-File.open("test.txt", "w") do |f|
-	f.puts 
-	f.puts hash
-	f.puts 
+
+arr = []
+h = {}
+hash.each do |key, value|
+  if key == "events" then
+    value.each do |arr|
+      event = Event.new
+      event.url = arr["event_url"]
+      event.title = arr["title"]
+      event.description = arr["description"]
+      puts event
+    end
+  end 
 end
